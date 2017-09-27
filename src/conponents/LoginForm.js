1edbdfcb8468faa6ff5d1364d5475bf7df647b1d
William <william.gaiteiro@gmail.com>
2017-09-27 19:44:39 -0300

    Adicionado componente de login e menuLeft()

--- src/conponents/LoginForm.js
+++ src/conponents/LoginForm.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, {Text} from 'react'
 import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
 
 const LoginForm = () => (
   <div className='login-form'>
+      
     {/*
       Heads up! The styles below are necessary for the correct render of this example.
       You can do same with CSS, the main idea is that all the elements up to the `Grid`
@@ -14,16 +15,20 @@ const LoginForm = () => (
       body > div > div > div.login-form {
         height: 100%;
       }
+
     `}</style>
+
+
     <Grid
       textAlign='center'
-      style={{ height: '100%' }}
+      class="landing-image"
+      style={{ height: '100%', backgroundImage: 'url(/resources/images/background_login.jpg)' }}
       verticalAlign='middle'
     >
       <Grid.Column style={{ maxWidth: 450 }}>
-        <Header as='h2' color='teal' textAlign='center'>
-          <Image src='/logo.png' />
-          {' '}Log-in to your account
+
+        <Header as='h2' color='green' textAlign='center'>
+        <strong >Face</strong>Farm
         </Header>
         <Form size='large'>
           <Segment stacked>
@@ -31,21 +36,21 @@ const LoginForm = () => (
               fluid
               icon='user'
               iconPosition='left'
-              placeholder='E-mail address'
+              placeholder='Endereço de e-mail'
             />
             <Form.Input
               fluid
               icon='lock'
               iconPosition='left'
-              placeholder='Password'
+              placeholder='Senha'
               type='password'
             />
 
-            <Button color='teal' fluid size='large'>Login</Button>
+            <Button color='green' fluid size='large'>Entrar</Button>
           </Segment>
         </Form>
         <Message>
-          New to us? <a href='#'>Sign Up</a>
+          Novo por aqui? <a href='#'>Abra uma conta</a>
         </Message>
       </Grid.Column>
     </Grid>
