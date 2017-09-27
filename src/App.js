8945aa85bb40e2bf1faa41cfd0db69faa362b273
William <william.gaiteiro@gmail.com>
2017-09-27 13:25:33 -0300

    Added semantic-ui-react t

--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import './semantic/dist/semantic.min.css';
+
+import { Button } from 'semantic-ui-react'
+import { Menu } from 'semantic-ui-react'
+
 class App extends Component {
   render() {
+    const { activeItem } = this.state || {}
+    
+
     return (
       <div className="App">
         <div className="App-header">
@@ -13,6 +21,49 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Menu vertical>
+        <Menu.Item>
+          <Menu.Header>Products</Menu.Header>
+
+          <Menu.Menu>
+            <Menu.Item name='enterprise' active={activeItem === 'enterprise'} onClick={this.handleItemClick} />
+            <Menu.Item name='consumer' active={activeItem === 'consumer'} onClick={this.handleItemClick} />
+          </Menu.Menu>
+        </Menu.Item>
+
+        <Menu.Item>
+          <Menu.Header>CMS Solutions</Menu.Header>
+
+          <Menu.Menu>
+            <Menu.Item name='rails' active={activeItem === 'rails'} onClick={this.handleItemClick} />
+            <Menu.Item name='python' active={activeItem === 'python'} onClick={this.handleItemClick} />
+            <Menu.Item name='php' active={activeItem === 'php'} onClick={this.handleItemClick} />
+          </Menu.Menu>
+        </Menu.Item>
+
+        <Menu.Item>
+          <Menu.Header>Hosting</Menu.Header>
+
+          <Menu.Menu>
+            <Menu.Item name='shared' active={activeItem === 'shared'} onClick={this.handleItemClick} />
+            <Menu.Item name='dedicated' active={activeItem === 'dedicated'} onClick={this.handleItemClick} />
+          </Menu.Menu>
+        </Menu.Item>
+
+        <Menu.Item>
+          <Menu.Header>Support</Menu.Header>
+
+          <Menu.Menu>
+            <Menu.Item name='email' active={activeItem === 'email'} onClick={this.handleItemClick}>
+              E-mail Support
+            </Menu.Item>
+
+            <Menu.Item name='faq' active={activeItem === 'faq'} onClick={this.handleItemClick}>
+              FAQs
+            </Menu.Item>
+          </Menu.Menu>
+        </Menu.Item>
+      </Menu>
       </div>
     );
   }
