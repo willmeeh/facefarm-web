ae0478ed608cc0aaf31f971d2b63e4d8b28e154d
Arthur Hoch <arthur.j.h96@gmail.com>
2017-09-27 19:46:06 -0300

    Add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,70 +1,41 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
 import logo from './logo.svg';
 import './App.css';
-
-import './semantic/dist/semantic.min.css';
 
 import { Button } from 'semantic-ui-react'
 import { Menu } from 'semantic-ui-react'
 
+import LoginForm from './components/LoginForm'
+import NotFound from './components/NotFoud'
+
 class App extends Component {
   render() {
     const { activeItem } = this.state || {}
-    
+
 
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Router>
+        <div>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/topics">Topics</Link></li>
+          </ul>
+
+          <hr />
+
+          <Route exact path="/" component={LoginForm} />
+          <Route path="/about" component={LoginForm} />
+          <Route path="/topics" component={LoginForm} />
+          <Route component={NotFound}/>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Menu vertical>
-        <Menu.Item>
-          <Menu.Header>Products</Menu.Header>
-
-          <Menu.Menu>
-            <Menu.Item name='enterprise' active={activeItem === 'enterprise'} onClick={this.handleItemClick} />
-            <Menu.Item name='consumer' active={activeItem === 'consumer'} onClick={this.handleItemClick} />
-          </Menu.Menu>
-        </Menu.Item>
-
-        <Menu.Item>
-          <Menu.Header>CMS Solutions</Menu.Header>
-
-          <Menu.Menu>
-            <Menu.Item name='rails' active={activeItem === 'rails'} onClick={this.handleItemClick} />
-            <Menu.Item name='python' active={activeItem === 'python'} onClick={this.handleItemClick} />
-            <Menu.Item name='php' active={activeItem === 'php'} onClick={this.handleItemClick} />
-          </Menu.Menu>
-        </Menu.Item>
-
-        <Menu.Item>
-          <Menu.Header>Hosting</Menu.Header>
-
-          <Menu.Menu>
-            <Menu.Item name='shared' active={activeItem === 'shared'} onClick={this.handleItemClick} />
-            <Menu.Item name='dedicated' active={activeItem === 'dedicated'} onClick={this.handleItemClick} />
-          </Menu.Menu>
-        </Menu.Item>
-
-        <Menu.Item>
-          <Menu.Header>Support</Menu.Header>
-
-          <Menu.Menu>
-            <Menu.Item name='email' active={activeItem === 'email'} onClick={this.handleItemClick}>
-              E-mail Support
-            </Menu.Item>
-
-            <Menu.Item name='faq' active={activeItem === 'faq'} onClick={this.handleItemClick}>
-              FAQs
-            </Menu.Item>
-          </Menu.Menu>
-        </Menu.Item>
-      </Menu>
-      </div>
+      </Router>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './semantic/dist/semantic.min.css';
 import './index.css';
 import App from './App';
-import LoginForm from './conponents/LoginForm';
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<LoginForm />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
