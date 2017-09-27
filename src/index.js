b23bff0509677a30022d155a99da53456cf1cd72
William <william.gaiteiro@gmail.com>
2017-09-27 18:18:37 -0300

    Criado estrutura das pastas

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import LoginForm from './conponents/LoginForm';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<LoginForm />, document.getElementById('root'));
+
