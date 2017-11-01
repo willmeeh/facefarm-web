d762dcc777d2786d09a1e4f8aa80a534db61de03
Arthur Hoch <arthur.j.h96@gmail.com>
2017-11-01 12:59:36 -0200

    Update LoginApi.js

--- src/fetchs/LoginApi.js
+++ src/fetchs/LoginApi.js
@@ -4,18 +4,20 @@ class LoginApi {
 
     fetch('http://localhost:4000/auth/users', {
       method: "POST",
-      headers: new Headers({ "Content-Type": "application/json" }),
+      headers: {"Content-Type": "application/json"},
       body: JSON.stringify(credentials)
 
-    }).then(response => {
-      console.log('teste');
-      console.log(response);
-      console.log(response.json());
-
+    }).then(response => {     
       return response.json();
+      
+    }).then(function(responseJson) {
+       console.log(responseJson);
+       return responseJson;
+      
     }).catch(error => {
       console.log(error);
       return error;
+      
     });
   }
 }
